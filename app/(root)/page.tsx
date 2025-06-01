560963ef9df324703de5f72a7f4df164d9c3d9f1
Khushi <kaybesky89@gmail.com>
2025-06-01 21:02:02 +0530

    Everything ok

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,22 @@
 import InterviewCard from '@/components/InterviewCard'
 import { Button } from '@/components/ui/button'
 import { dummyInterviews } from '@/constants'
+import { getCurrentUser } from '@/lib/actions/auth.action'
+import { getInterviewsByUserId, getLatestInterviews} from '@/lib/actions/general.action'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-function Page() {
+async function Page() {
+  const user = await getCurrentUser();
+  const [userInterviews, latestInterviews]=await Promise.all([
+    await getInterviewsByUserId(user?.id!),
+    await getLatestInterviews({userId: user?.id!})
+  ])
+
+  const hasPastInterviews = userInterviews?.length! > 0;
+  const hasUpcomingInterviews = latestInterviews?.length! > 0;
+
   return (
     <>
       <section className='card-cta'>
@@ -26,9 +37,13 @@ function Page() {
         <h2>Your Interviews</h2>
 
         <div className='interviews-section'>
-         {dummyInterviews.map((interview)=>(
-          <InterviewCard {...interview} key={interview.id}/>
-         ))}
+          {hasPastInterviews ? (
+            userInterviews?.map((interview)=>(
+              <InterviewCard {...interview} key={interview.id}/>
+          ))):(
+            <p>You haven't taken any interviews yet.</p>
+          )}
+         
         </div>
       </section>
 
@@ -36,7 +51,12 @@ function Page() {
         <h2>Take an Interview</h2>
 
         <div className='interviews-section'>
-          <p>There are no interviews available</p>
+        {hasUpcomingInterviews ? (
+            latestInterviews?.map((interview)=>(
+              <InterviewCard {...interview} key={interview.id}/>
+          ))):(
+            <p>There are no new interviews available.</p>
+          )}
         </div>
       </section>
     </>
